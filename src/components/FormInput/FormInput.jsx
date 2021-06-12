286b11123cd221b278baa786f39bec5010565b76
Abbas Salami <olawalesalami3@gmail.com>
2021-06-12 15:03:30 +0100

    signup page

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,51 +1,69 @@
-import React, { useState } from 'react';
-import { TextField, InputAdornment, IconButton } from '@material-ui/core';
+import React, { useState } from "react";
+import { TextField, InputAdornment, IconButton } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
 import { Visibility } from "@material-ui/icons";
 import { VisibilityOff } from "@material-ui/icons";
-import { useField } from 'formik';
+import { useField } from "formik";
 
-import './FormInput.css';
+import "./FormInput.css";
 
-const FormInput = ({ name, type, ...otherProps }) => {
-    const [field, meta] = useField(name);
-    const [showPassword, setShowPassword] = useState(false)
+const styles = (theme) => ({
+  root: {
+    "&:hover:not($disabled):not($focused):not($error) $notchedOutline": {
+      borderColor: "#3659B5",
+    },
+    color: "#0E101A",
+    borderColor: "#79809767",
+  },
+  disabled: {},
+  focused: {},
+  error: {},
+  notchedOutline: {},
+});
 
-    const configTextField = {
-        ...field,
-        ...otherProps,
-        fullWidth: true,
-        variant: 'outlined'
-    }
+const FormInput = ({ name, type, classes, ...otherProps }) => {
+  const [field, meta] = useField(name);
+  const [showPassword, setShowPassword] = useState(false);
 
-    if (meta && meta.touched && meta.error) {
-        configTextField.error = true;
-        configTextField.helperText = meta.error;
-    }
+  const configTextField = {
+    ...field,
+    ...otherProps,
+    fullWidth: true,
+    variant: "outlined",
+  };
 
-    let inputProps;
-    if (type === 'password') {
-        inputProps = {
-            endAdornment: (
-                <InputAdornment position="end">
-                    <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={() => setShowPassword(!showPassword)}
-                    >
-                        {showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                </InputAdornment>
-            )
-        }
-    }
+  if (meta && meta.touched && meta.error) {
+    configTextField.error = true;
+    configTextField.helperText = meta.error;
+  }
 
-    return (
-        <TextField 
-            {...configTextField}
-            className="formInput"
-            InputProps={inputProps}
-            type={showPassword ? 'text' : type}
-        />
-    );
+  let inputProps;
+  if (type === "password") {
+    inputProps = {
+      endAdornment: (
+        <InputAdornment position="end">
+          <IconButton
+            aria-label="toggle password visibility"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <Visibility /> : <VisibilityOff />}
+          </IconButton>
+        </InputAdornment>
+      ),
+    };
+  }
+
+  return (
+    <TextField
+      {...configTextField}
+      className="formInput"
+      InputProps={{
+        ...inputProps,
+        classes: classes,
+      }}
+      type={showPassword ? "text" : type}
+    />
+  );
 };
 
-export default FormInput;
+export default withStyles(styles)(FormInput);
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -4,16 +4,20 @@ import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 
 import FormInput from '../../components/FormInput/FormInput';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { Button } from "@material-ui/core";
+import Checkbox from '@material-ui/core/Checkbox';
 import './SignUp.css';
 
-const SignUp = () => {
+const SignUp = ({history}) => {
 
     const INITIAL_FORM_STATE = {
         username: '',
         phone: '',
         referralCode: '',
         password: '',
-        confirmPassword: ''
+        confirmPassword: '',
+        toc: false
     };
 
     const formValidation = Yup.object().shape({
@@ -29,35 +33,70 @@ const SignUp = () => {
             .required('Password is required'),
         confirmPassword: Yup.string()
             .oneOf([Yup.ref('password'), null], 'Passwords must match')
-            .required('Correct password is required')
+        .required('Correct password is required'),
+        toc: Yup.bool().required()
     })
 
     return (
-        <div className="signup p-3">
-            <Link to="/" className="logo">GAME</Link>
+      <div className="signup p-3">
+        <Link to="/" className="logo">
+          GAME
+        </Link>
 
-            <div className="">
-                <h1>Get Started</h1>
-                <p>Create your free account on the game</p>
-            </div>
-
-            <>
-                <Formik
-                    initialValues={{ ...INITIAL_FORM_STATE }}
-                    validationSchema={formValidation}
-                    onSubmit={values => {
-                        console.log(values)
-                    }}
-                >
-                    <Form>
-                        <FormInput name="username" label="Username" />
-                        <FormInput name="referralCode" label="Referral Code (Optional)" />
-                        <FormInput name="password" label="Password" type="password" />
-                        <FormInput name="confirmPassword" label="Confirm Password" type="password"/>
-                    </Form>                    
-                </Formik>
-            </>
+        <div className="signup__intro">
+          <h1>Get Started</h1>
+          <p>Create your free account on the game</p>
         </div>
+
+        <>
+          <Formik
+            initialValues={{ ...INITIAL_FORM_STATE }}
+            validationSchema={formValidation}
+            onSubmit={(values) => {
+              console.log('hi');
+            }}
+          >
+            <Form onSubmit={(e) => {
+              e.preventDefault();
+              history.push('/login')
+            }}>
+              <FormInput name="username" label="Username" />
+              <FormInput name="referralCode" label="Referral Code (Optional)" />
+              <FormInput name="password" label="Password" type="password" />
+              <FormInput
+                name="confirmPassword"
+                label="Confirm Password"
+                type="password"
+              />
+              <FormControlLabel
+                control={<Checkbox name="toc" color="primary" />}
+                label={
+                  <p>
+                    I read and accept
+                    <span> Terms of Service & Privacy Policy</span>
+                  </p>
+                }
+                className="signup__toc"
+              />
+              <Button
+                variant="contained"
+                fullWidth={true}
+                color="primary"
+                type="submit"
+                size="large"
+              >
+                Get Started
+              </Button>
+            </Form>
+          </Formik>
+          <div className="signup__redirect">
+            <p>
+              Got an account? <Link to="/login">Log in</Link>
+            </p>
+            <p className="copyright">All rights reserved © 2020 The Base</p>
+          </div>
+        </>
+      </div>
     );
 };
 
